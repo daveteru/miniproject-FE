a41a177b2b7e8ab0afed6c025f5b4e6fc736b450
Teru <218044131+daveteru@users.noreply.github.com>
2026-04-02 10:33:20 +0700

    refactor : cards, button & sections

--- src/components/Featuredcard.tsx
+++ src/components/Featuredcard.tsx
@@ -1,6 +1,9 @@
 export default function Featuredcard() {
   return (
-    <div className="flex flex-col gap-2">
+    <div
+      className="flex flex-col gap-2  hover:shadow-[0px_4px_6px_0px_rgba(0,0,0,0.1)]
+ p-2 rounded-lg transition-all ease-in"
+    >
       <div className=" bg-gray-400 w-full h-[350px] rounded-xl"></div>
       <div className="flex justify-between w-full">
         <div>
--- src/components/Landingpagefeatured.tsx
+++ src/components/Landingpagefeatured.tsx
@@ -6,7 +6,7 @@ export default function Landingpagefeatured() {
       {" "}
       <div className=" container mx-auto px-5 md:px-15 lg:px-30 my-10">
         <h1 className="text-2xl mb-5">FEATURED</h1>
-        <div className="grid lg:grid-cols-2 lg:grid-rows-2 gap-5 ">
+        <div className="grid lg:grid-cols-2 lg:grid-rows-2 gap-3 ">
           <Featuredcard />
           <Featuredcard />
           <Featuredcard />
