eace678962dbe6f4a97f00719a59752bde0020cb
Teru <218044131+daveteru@users.noreply.github.com>
2026-04-15 00:26:25 +0700

    feat: wired front end transaction to API

--- src/pages/Transaction.tsx
+++ src/pages/Transaction.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router";
+import { useNavigate, useSearchParams } from "react-router";
 import discountticket from "../assets/icons/discount.svg";
+import Cartcard from "../components/Cartcard";
 import { Smalldetailstransaction } from "../components/Smalldetails";
 import TicketCard, { type CartItem } from "../components/TicketCard";
 import Toggler from "../components/Toggler";
 import { axiosInstance } from "../lib/axios";
-import Cartcard from "../components/Cartcard";
+import { useAppStore } from "../store/useAppStore";
 import { formatThousand } from "../utility/dateconvert";
-
 
 type Ticket = {
   id: number;
@@ -42,6 +42,7 @@ type eventdetails = {
     avatar: string | null;
   };
   vouchers: {
+    id: number;
     expiredDate: string;
     discamount: number;
     amount: number;
@@ -49,33 +50,26 @@ type eventdetails = {
 };
 
 type transactiondetails = {
-  id: number;
-  uuid: string;
-  expiredAt: string;
-  paymentProof: string;
-  paymentStatus: string;
   userId: string;
-  ticketId: string;
-  voucherId: string;
-  couponId: number;
-  pointsUsed: string;
-  tickets: {
-    id: number;
-    ticketLevel: string;
-    qty: number;
-  }[];
-};
+  items: [
+    {
+      ticketId: number;
+      quantity: number;
+    },
+  ];
+}[];
 
 export default function Transaction() {
   const [isvoucher, setIsvoucher] = useState<boolean>(false);
   const [tickets, setTickets] = useState<Ticket[]>([]);
-  const [eventdetails,setEventdetails] = useState<eventdetails | null>(null)
+  const [eventdetails, setEventdetails] = useState<eventdetails | null>(null);
   const [searchParams] = useSearchParams();
   const [cart, setCart] = useState<CartItem[]>([]);
+  const [isloading, setIsloading] = useState(false);
   const [total, setTotal] = useState<number>(0);
-  const [transaction,setTransaction] = useState<transactiondetails|null>(null)
-
-
+  const [cartResetKey, setCartResetKey] = useState(0);
+  const { user } = useAppStore();
+  const navigate = useNavigate()
   const eventId = searchParams.get("eventId");
 
   useEffect(() => {
@@ -91,19 +85,56 @@ export default function Transaction() {
     if (eventId) fetchTickets();
   }, [eventId]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const subtotal = cart.reduce((a, item) => a + item.price * item.qty, 0);
-    const voucherdiscount = eventdetails?.vouchers[0].discamount ?? 0
-    if (isvoucher){
-     return  setTotal(subtotal-voucherdiscount)
+    const voucherdiscount = eventdetails?.vouchers[0].discamount ?? 0;
+    if (isvoucher) {
+      return setTotal(subtotal - voucherdiscount);
     }
-    return setTotal(subtotal)
-  },[cart,isvoucher]);
+    return setTotal(subtotal);
+  }, [cart, isvoucher]);
+
+  const processTransaction = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (cart.length === 0) {
+      alert("Cart must not be empty");
+      return;
+    }
+    setIsloading(true);
+    try {
+      const voucherPayload = isvoucher
+        ? { voucherId: eventdetails?.vouchers[0].id }
+        : {};
+      const payload = {
+        userId: Number(user?.id),
+        ...voucherPayload,
+        items: cart.map(({ id: ticketId, qty: quantity }) => ({
+          ticketId,
+          quantity,
+        })),
+      };
+      await axiosInstance.post("/transactions", payload);
+      alert("Submission sucess")
+      setCart([])
+      navigate(`/events/${eventId}`);
+    } catch (err: any) {
+      alert("Submission failed");
+      console.log(
+        err.response?.data?.errors ??
+          err.response?.data?.message ??
+          err.message,
+      );
+    } finally {
+      setIsloading(false);
+    }
+  };
 
   return (
     <div className="w-full mih-h-screen border border-transparent bg-neutral-100 h-min-screen ">
       <section className="bg-white container mx-auto w-full max-w-250 h-full mt-20 rounded-t-2xl border-t border-x overflow-hidden border-neutral-300 ">
-        <div className="w-full h-80 bg-neutral-400 overflow-hidden "><img src={eventdetails?.thumbnail} alt="" className="object-cover " /></div>
+        <div className="w-full h-80 bg-neutral-400 overflow-hidden ">
+          <img src={eventdetails?.thumbnail} alt="" className="object-cover " />
+        </div>
         <div className="flex w-full text-center items-center mt-4 justify-center p-2">
           <hr className="flex-1 border-neutral-200 mx-5" />
           <p className="text-neutral-500">{eventdetails?.name}</p>
@@ -116,7 +147,7 @@ export default function Transaction() {
             <div className="flex flex-col gap-4">
               {tickets.map((t) => (
                 <TicketCard
-                  key={t.id}
+                  key={`${t.id}-${cartResetKey}`}
                   id={t.id}
                   ticketLevel={t.ticketLevel}
                   price={t.price}
@@ -130,11 +161,15 @@ export default function Transaction() {
             <div className="w-full h-fit border rounded-lg border-neutral-300 p-5 drop-shadow-lg flex flex-col justify-between bg-white">
               <div className="flex flex-col gap-2">
                 <h1>{eventdetails?.name}</h1>
-                <Smalldetailstransaction location={eventdetails?.location} city={eventdetails?.city} date={eventdetails?.startDate}/>
+                <Smalldetailstransaction
+                  location={eventdetails?.location}
+                  city={eventdetails?.city}
+                  date={eventdetails?.startDate}
+                />
                 <hr className="border-neutral-300"></hr>
                 {/* CURRENT TRANSACTION */}
                 <h1>Current Transaction</h1>
-                <div className="w-full h-fit min-h-20 border-2 border-dashed p-2 flex flex-col gap-2 border-neutral-300 rounded-2xl mb-2">
+                <div className="w-full h-fit  border-2 border-dashed p-2 flex flex-col gap-2 border-neutral-300 rounded-2xl ">
                   {cart.map((cart) => (
                     <Cartcard
                       ticketLevel={cart.ticketLevel}
@@ -142,13 +177,38 @@ export default function Transaction() {
                       price={cart.price}
                     />
                   ))}
+                  {cart.length <= 0 ? (
+                    <p className="text-center text-neutral-300">
+                      No tickets in cart..
+                    </p>
+                  ) : (
+                    ""
+                  )}
                 </div>
+                {cart.length <= 0 ? (
+                  ""
+                ) : (
+                  <button
+                    onClick={() => {
+                      setCart([]);
+                      setCartResetKey((k) => k + 1);
+                    }}
+                    className="underline hover:text-red-600 cursor-pointer text-sm"
+                  >
+                    Clear Cart
+                  </button>
+                )}
 
                 <hr className="border-neutral-300"></hr>
                 {isvoucher ? (
                   <div className="items-center flex justify-between">
                     <small>Voucher Discount</small>
-                    <strong>-IDR {formatThousand(eventdetails?.vouchers[0].discamount?? 0)}</strong>
+                    <strong>
+                      -IDR{" "}
+                      {formatThousand(
+                        eventdetails?.vouchers[0].discamount ?? 0,
+                      )}
+                    </strong>
                   </div>
                 ) : (
                   ""
@@ -158,9 +218,18 @@ export default function Transaction() {
                   <strong>IDR {formatThousand(total)}</strong>
                 </div>
               </div>
-              <button className="bg-[#E6FF06] font-krona-one px-5 py-2 mt-3 rounded-md w-full">
-                Confirm Purchase
-              </button>
+              {isloading ? (
+                <button className="bg-[#aaaaaa] font-krona-one px-5 py-2 mt-3 rounded-md w-full">
+                  Loading
+                </button>
+              ) : (
+                <button
+                  onClick={processTransaction}
+                  className="bg-[#E6FF06] font-krona-one px-5 py-2 mt-3 rounded-md w-full hover:bg-amber-300 transition-colors ease-in cursor-pointer"
+                >
+                  Confirm Purchase
+                </button>
+              )}
             </div>
             <div className="w-full h-20 border  justify-between rounded-lg text-neutral-500 font-[inter] flex items-center border-neutral-300 p-5 drop-shadow-lg bg-white">
               <img src={discountticket} className="mr-2" />
