fd197c19682f860434528988b7b38a714d1b891f
Teru <218044131+daveteru@users.noreply.github.com>
2026-04-19 06:12:38 +0700

    fix: added discamount & amount fileds at create page

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,18 +1,22 @@
-export default function Review() {
+type reviewprops = {
+  text: string;
+  rating: number;
+  reviewername: string;
+  revieweravatar: string;
+};
+
+export default function Review({ text, rating, reviewername, revieweravatar }: reviewprops) {
   return (
     <div className="border-t border-gray-200 flex items-center p-5 gap-5">
-      <img className="w-15 h-15 rounded-full bg-black"></img>
+      <img src={revieweravatar} className="w-15 h-15 rounded-full bg-black object-cover"></img>
       <div className="flex-1">
         <div className="flex gap-2">
-          {" "}
-          <h1>username</h1>•<span className="text-amber-500">★★★★</span>
+          <h1>{reviewername}</h1>•
+          <span className="text-amber-500">
+            {"★".repeat(rating)}{"☆".repeat(5 - rating)}
+          </span>
         </div>
-        <p>
-          {" "}
-          Lorem ipsum dolor sit amet consectetur. Vestibulum massa purus eu
-          iaculis arcu. Platea ultrices urna ultrices nullam nullam auctor
-          potenti. Vulputate fermentum nulla lobortis arcu sceleris
-        </p>
+        <p>{text}</p>
       </div>
     </div>
   );
